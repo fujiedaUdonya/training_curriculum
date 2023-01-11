8ccf964e90e4bb8b07de105be9f91dcab9695c08
fujieUdonya <suizouharetsu1110@hotmail.co.jp>
2023-01-11 13:54:27 +0900

    Issue4~6の解消 の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,7 +3,7 @@ class CalendarsController < ApplicationController
   # １週間のカレンダーと予定が表示されるページ
   def index
     getWeek
-    @plan = Plan.all
+    @plan = Plan.new
   end
 
   # 予定の保存
@@ -44,8 +44,8 @@ class CalendarsController < ApplicationController
       if wday_num >= 7 #「wday_numが7以上の場合」という条件式
         wday_num = wday_num - 7
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day,
-               :plans => today_plans,:wday => wdays[wday_num] }
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day,
+               plans: today_plans, wday:  wdays[wday_num] }
                @week_days.push(days)
                
     end
